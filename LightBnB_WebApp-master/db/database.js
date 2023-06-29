01d366f7dae2aa9fda043c96d714c7bb987add91
bzrkrbddy081 <bzrkrbddy081@gmail.com>
2023-06-29 18:22:54 +0000

    finished Users

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,6 +1,17 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+// pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)})
+
 /// Users
 
 /**
@@ -8,16 +19,37 @@ const users = require("./json/users.json");
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
+
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user?.email.toLowerCase() === email?.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  return pool
+  .query(`SELECT *
+  FROM users
+  WHERE email = $1 LIMIT 1`, [email])
+  .then((result) => {
+    // console.log('getUserWithEmail');
+    // console.log(result.rows);
+    // console.log(result.rows[0]);
+    if (result.rows.length > 0) {
+      return result.rows[0];
+    } else {
+      return null;
+      }
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 };
+
+// const getUserWithEmail = function (email) {
+//   let resolvedUser = null;
+//   for (const userId in users) {
+//     const user = users[userId];
+//     if (user?.email.toLowerCase() === email?.toLowerCase()) {
+//       resolvedUser = user;
+//     }
+//   }
+//   return Promise.resolve(resolvedUser);
+// };
 
 /**
  * Get a single user from the database given their id.
@@ -25,8 +57,26 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+  .query(`SELECT * FROM users
+  WHERE users.id = $1 LIMIT 1`, [id])
+  .then((result) => {
+    // console.log('getUserWithID');
+    // console.log(result.rows);
+    if (result.rows.length > 0) {
+      return result.rows[0];
+    } else {
+      return null
+    }
+  })
+  .catch((error) => {
+    console.log(error.message)
+  });
 };
+
+// const getUserWithId = function (id) {
+//   return Promise.resolve(users[id]);
+// };
 
 /**
  * Add a new user to the database.
@@ -34,11 +84,24 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+  .query(`INSERT INTO users (name, email, password)
+  VALUES ($1,$2,$3) RETURNING *;`,[user.name, user.email, user.password])
+  .then((result) => {
+    console.log('addUSER');
+    console.log(result.rows);
+   return result.rows; 
+  })
+  .catch((error) => console.log(error.message));
 };
+
+// const addUser = function (user) {
+//   const userId = Object.keys(users).length + 1;
+//   user.id = userId;
+//   users[userId] = user;
+//   return Promise.resolve(user);
+// };
+
 
 /// Reservations
 
@@ -59,13 +122,20 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(`SELECT * 
+    FROM properties
+    LIMIT $1`, [limit])
+    .then((result) => {
+      // console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
+//two returns pool and results.row
 
 /**
  * Add a property to the database
